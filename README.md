38bdd104a6aa2baa9c484e767104db078dd61b84
Tomas Dobrotka <tomas@dobrotka.sk>
2017-10-15 14:14:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# ipwfl - IP webservice frontend list generator
+
+Build IP address list of web services like google, slack, github... Then generate iptables or other firewall rules.
+
+**Version:** 0.01 Experimental
+
+
+## Features:
+ - [x] Generate rules from static IP list
+ - [x] Convert custom IP/Net list to ipconfig template (firewall rule)
+ - [x] ASN list lookup
+ - [x] Autodiscover ASN, based on hostname. [eg. github.com, youtube.com, ...]
+
+ 
+
+Some notes: Services like slack.com are hosted on Amazon, that's why ASN covers other services, when using file 'services_list.txt'
+
+## Why?
+- Imagine you want to block all traffic on your home network, but you want your Android TV to access google services.
+- Imagine you want to block all traffic on your work network, but you want to access github.com, Slack.com, and some other custom IPs.
+- This scripts generates IPs list based on the following input parameters: IP list, ASN, Domain2ASN
+
+
+## Usage
+
+
+- 1. Git clone
+- 2. Edit or remove content from 'prefix.txt' (in the ./data directory)
+- 3. Edi the line 'OUTPUT_COMMAND_TEMPLATE' in 'ipwfl.py' to match your network FW rules (allow/drop...)
+- 5. Then run:
+
+> 'python ./ipwfl.py'
+
+When everything works fine. List with IP-ranges will bee generated.
+Customise the content of .txt files in the /data directory to match your needs.
+
+
+
+## Input Files:
+
+
+### - 'prefix.txt'
+Basic iptables/FW rules. Will be used as RAW 1:1 output on start.
+
+
+### -static_list.txt  
+Static IP/Network list - can put IP/Networks one per line. Lines starting on '#' are comments.
+
+
+### - 'asn_list.txt'
+List of ASN to lookup and add to final IP list.
+https://en.wikipedia.org/wiki/Autonomous_system_(Internet)
+
+
+### - 'services_list.txt'
+List of domain names. (github.com, facebook.com, ...) will try to resolve IP/ASN/IP-ranges, routes... for the domain.
+
+
+
+
+
+
+## TODO:
+ - [ ] TODO: remove duplicates in output!
+ - [ ] normalize readme.md check typos, ...
---
+++ ipwfl.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#template for ipconfig line generator
+# OUTPUT_COMMAND_TEMPLATE='%%NETWORK%%' # TO OUTPUT IP/RANGES ONLY.
+OUTPUT_COMMAND_TEMPLATE='iptables -YOUR -RULE -HERE -j ALLOW -d %%NETWORK%%' # CHANGE THIS TO YOUR IPCONFIG RULE!!!
+
+TMP_FILE='./tmp.work.txt'
+AS_ROUTE_LOOKUP_CMD_TEMPLATE="whois -h whois.radb.net -- '-i origin %%AS%%' | grep 'route:' > "+TMP_FILE
+IP_TO_ASN_LOOKUP_CMD_TEMPLATE="whois -h whois.radb.net -- '-i origin %%IP%%' | grep 'origin:' > "+TMP_FILE
+
+import os
+import socket
+
+
+def append_to_output(LINE):
+    print LINE
+
+
+def add_line_to_output(NETWORK):
+
+    if (NETWORK[:1] != '#' and NETWORK[:1] != ''):
+        OUTPUT_LINE_TMP=OUTPUT_COMMAND_TEMPLATE.replace('%%NETWORK%%',str(NETWORK))
+        append_to_output(OUTPUT_LINE_TMP)
+    else:
+        append_to_output(NETWORK)
+
+
+
+def read_txt(FILE):
+    FILE_FULLPATH=str(FILE)
+    text_file = open(FILE_FULLPATH, "r")
+    LINES = text_file.readlines()
+    for line in LINES:
+        append_to_output(line.strip())
+    text_file.close()
+    add_line_to_output('')
+
+def find_asn_in_whois_output(FILE):
+    text_file = open(FILE, "r")
+    LINES = text_file.readlines()
+    for line in LINES:
+        line=line.strip()
+        if (line[:7].strip()=='origin:'):
+            text_file.close()
+            return line.replace('origin:','').strip()
+
+
+
+
+def read_txt_static(FILE):
+    FILE_FULLPATH=str(FILE)
+
+    text_file = open(FILE_FULLPATH, "r")
+    LINES = text_file.readlines()
+    for line in LINES:
+        line=line.strip()
+        if (line[:1]!='#' and line[:1]!=''):
+            line = line.replace('route:', '').strip()
+            add_line_to_output(line)
+    text_file.close()
+    add_line_to_output('')
+
+
+def read_asn_file_and_lookup_routes(FILE):
+    FILE_FULLPATH=str(FILE)
+
+    text_file = open(FILE_FULLPATH, "r")
+    LINES = text_file.readlines()
+    for line in LINES:
+        line=line.strip()
+        if (line[:1]!='#' and line[:1]!=''):
+            lookup_asn_route(line)
+    text_file.close()
+    add_line_to_output('')
+
+def read_service_by_domain(FILE_FULLPATH):
+    text_file = open(FILE_FULLPATH, "r")
+    LINES = text_file.readlines()
+    for line in LINES:
+        line=line.strip()
+        if (line[:1]!='#' and line[:1]!=''):
+            ip_to_asn(hostname_to_asn_routes(line.strip()))
+    text_file.close()
+    add_line_to_output('')
+
+
+def lookup_asn_route(NET_AS_LINE):
+    NET_AS=NET_AS_LINE.split('#')[0].strip()
+    add_line_to_output('#ASN: '+str(NET_AS_LINE))
+    COMMAND=AS_ROUTE_LOOKUP_CMD_TEMPLATE
+    COMMAND=COMMAND.replace('%%AS%%',str(NET_AS))
+    os.system(COMMAND)
+    read_txt_static(TMP_FILE)
+    add_line_to_output('')
+    remove_file(TMP_FILE)
+
+def ip_to_asn(IP):
+    COMMAND=IP_TO_ASN_LOOKUP_CMD_TEMPLATE
+    COMMAND=COMMAND.replace('%%IP%%',str(IP))
+    os.system(COMMAND)
+    ASN=find_asn_in_whois_output(TMP_FILE)
+    lookup_asn_route(str(ASN))
+    add_line_to_output('')
+    remove_file(TMP_FILE)
+
+
+def hostname_to_asn_routes(DOMAIN):
+    add_line_to_output('#HOSTNAME/ASN/IPs for : ' + str(DOMAIN))
+    return str(socket.gethostbyname(DOMAIN))
+
+def remove_file(FILE):
+    try:
+        os.remove(FILE)
+    except:
+        pass
+
+
+#---------------------------start-----------------------------
+
+remove_file(TMP_FILE)
+
+read_txt('./data/prefix.txt')
+# read_txt_static('./data/static_list.txt')
+# read_asn_file_and_lookup_routes('./data/asn_list.txt')
+read_service_by_domain('./data/services_list.txt')
+
+remove_file(TMP_FILE)
+
+
+
